fe0b6d3c87445f7cf7f2219631ee412eb66c6195
Cass <dev@cass.si>
2021-06-24 13:03:38 +0100

    add support for pull_request event type

--- src/github/ContextHelper.ts
+++ src/github/ContextHelper.ts
@@ -1,50 +1,54 @@
 import * as core from "@actions/core";
 import { normalize } from "../util/util";
 
-export type ActionType = "commit" | "tag" | "other";
+export type ActionType = "commit" | "tag" | "pr" | "other";
 
 export class ContextHelper {
-    private actionType: ActionType;
-    private normalizedRefName: string;
-    private refName: string;
+  private actionType: ActionType;
+  private normalizedRefName: string;
+  private refName: string;
 
-    /**
-     * Creates a new instance.
-     */
-    constructor() {
-        const githubRef = process.env.GITHUB_REF!;
-        if (githubRef.startsWith("refs/heads/")) {
-            this.refName = githubRef.substr(11);
-            this.actionType = "commit";
-        } else if (githubRef.startsWith("refs/tags/")) {
-            this.refName = githubRef.substr(10);
-            this.actionType = "tag";
-        } else {
-            core.warning(`WARNING: Unrecognized GITHUB_REF found: ${githubRef}.`);
-            this.refName = githubRef;
-            this.actionType = "other";
-        }
-        this.normalizedRefName = normalize(this.refName);
-    }
+  /**
+   * Creates a new instance.
+   */
+  constructor() {
+    const githubRef = process.env.GITHUB_REF!;
 
-    /**
-     * Returns the action type that caused this action to run.
-     */
-    public getActionType(): ActionType {
-        return this.actionType;
+    if (githubRef.startsWith("refs/pull/")) {
+      this.refName = process.env.GITHUB_HEAD_REF!;
+      this.actionType = "pr";
+    } else if (githubRef.startsWith("refs/heads/")) {
+      this.refName = githubRef.substr(11);
+      this.actionType = "commit";
+    } else if (githubRef.startsWith("refs/tags/")) {
+      this.refName = githubRef.substr(10);
+      this.actionType = "tag";
+    } else {
+      core.warning(`WARNING: Unrecognized GITHUB_REF found: ${githubRef}.`);
+      this.refName = githubRef;
+      this.actionType = "other";
     }
+    this.normalizedRefName = normalize(this.refName);
+  }
 
-    /**
-     * Returns the normalized ref name (tag or branch name usually).
-     */
-    public getNormalizedRefName(): string {
-        return this.normalizedRefName;
-    }
+  /**
+   * Returns the action type that caused this action to run.
+   */
+  public getActionType(): ActionType {
+    return this.actionType;
+  }
 
-    /**
-     * Returns the unchanged ref name (tag or branch name usually).
-     */
-    public getRefName(): string {
-        return this.refName;
-    }
+  /**
+   * Returns the normalized ref name (tag or branch name usually).
+   */
+  public getNormalizedRefName(): string {
+    return this.normalizedRefName;
+  }
+
+  /**
+   * Returns the unchanged ref name (tag or branch name usually).
+   */
+  public getRefName(): string {
+    return this.refName;
+  }
 }
